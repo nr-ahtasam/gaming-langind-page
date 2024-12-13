f95ade922c7daa593dd566f678d961b6ebf39581
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-12-14 01:11:11 +0600

    added video player with next-video and mux

--- app/components/VideoSection.js
+++ app/components/VideoSection.js
@@ -1,21 +1,21 @@
+import ReactPlayer from 'react-player';
+import Video from 'next-video';
+import Player from 'next-video/player';
 import thumbnail from '@/public/images/video-thumbnail.png'
+import video1 from '@/videos/video1.mp4.json'
 const VideoSection = () => {
   return (
     <div className="container mx-auto my-40">
       <article className="flex justify-between ">
-        <div className="w-1/2 ">
-          <img
-            alt=""
-            src={thumbnail.src}
-            className=" h-full w-full object-cover"
-          />
+        <div className="w-1/2">
+          <Video src={video1} />;
         </div>
 
         <div className="w-1/2 flex  flex-col justify-between">
           <div className="border-s border-gray-900/10 p-8 ">
             <div>
               <h3 className="font-bold capitalize text-4xl">
-               We are the Best Game <br/> Distrubutors for all Gamers.
+                We are the Best Game <br/> Distrubutors for all Gamers.
               </h3>
             </div>
 
